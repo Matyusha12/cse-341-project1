c87912f0f07e0e88c7d3ac6e2cf0df57442b9d0e
Matyusha12 <nataliyayakovleva90@icloud.com>
2024-05-18 22:50:22 -0600

    commit all

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,10 @@ router.get('/', usersController.getAll);
 
 router.get('/:id', usersController.getSingle);
 
+router.post('/', usersController.updateUser);
+
+router.put('/:id', usersController.updateUser);
+
+router.delete('/:id', usersController.deleteUser);
+
 module.exports = router;
